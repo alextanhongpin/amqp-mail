bccdc8a6a691b3a7d27afa94fda5f096c8a4ef88
Alex Tan Hong Pin <alextan220990@gmail.com>
2017-02-12 01:45:13 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# amqp-mail
-Email microservice with amqp
+# Email Microservice
+
+*Work in Progress*
+
+Creating a queue:
+
+```
+const newsletterQueue = channel.assertQueue(queue, { durable: false })
+
+newsletterQueue.then((q) => {
+  channel.consume(q.queue, (message) => {
+    console.log('[x] Received message from newsletter queue', message.content.toString())
+  }, { noAck: true })
+})
+
+const inviteQueue = channel.assertQueue('invite', { durable: false })
+
+inviteQueue.then((q) => {
+  channel.consume(q.queue, (message) => {
+    console.log('[x] Received message from invite queue', message.content.toString())
+  }, { noAck: true })
+})
+
+```
--- email-service/transport.js
+++ email-service/transport.js
@@ -24,21 +24,6 @@ module.exports = function init ({ amqpConn }) {
   // Bind consumer here
   amqpConn.createChannel().then((channel) => {
 
-    const newsletterQueue = channel.assertQueue(queue, { durable: false })
-
-    newsletterQueue.then((q) => {
-      channel.consume(q.queue, (message) => {
-        console.log('[x] Received message from newsletter queue', message.content.toString())
-      }, { noAck: true })
-    })
-
-    const inviteQueue = channel.assertQueue('invite', { durable: false })
-
-    inviteQueue.then((q) => {
-      channel.consume(q.queue, (message) => {
-        console.log('[x] Received message from invite queue', message.content.toString())
-      }, { noAck: true })
-    })
 
     const emailExchange = channel.assertExchange(exchange, 'topic', { durable: false })
 
